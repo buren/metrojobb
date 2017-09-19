bc9d09c0731830800e7f02f53713df0170ede4ec
Jacob Burenstam <burenstam@gmail.com>
2017-09-19 20:18:10 +0200

    Add Model#to_xml! method that raises an exception if the models is invalid

--- lib/metrojobb/model.rb
+++ lib/metrojobb/model.rb
@@ -3,5 +3,14 @@ require 'active_model'
 module Metrojobb
   class Model
     include ActiveModel::Model
+
+    InvalidError = Class.new(StandardError)
+
+    def to_xml!
+      return to_xml if valid?
+
+      error_message = "#{model_name} has errors on: #{self.errors.keys.join(', ')}"
+      raise(InvalidError, error_message)
+    end
   end
 end
