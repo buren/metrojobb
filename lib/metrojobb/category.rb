0a3ef6e45249751c8553e415dff5db128354b0df
Jacob Burenstam <burenstam@gmail.com>
2018-02-22 10:51:07 +0100

    hack

--- lib/metrojobb/category.rb
+++ lib/metrojobb/category.rb
@@ -5,9 +5,26 @@ module Metrojobb
   class Category < Model
     attr_accessor :id, :name
 
-    NAME_ID_MAP = CSV.read(
-      File.expand_path('../../../data/categories.csv', __FILE__)
-    ).to_h.invert.freeze
+    NAME_ID_MAP = lambda {
+      top_categories = {}
+
+      data = CSV.read(
+        File.expand_path('../../../data/categories.csv', __FILE__)
+      ).map do |row|
+        id, name = row
+        if id.length < 3
+          top_categories[id] = name
+        else
+          top_id = id[0..1]
+          top_id = id[0] if id.length == 3
+          top_name = top_categories[top_id]
+          name = "#{top_name} > #{name}"
+        end
+        [id, name]
+      end
+
+      data.to_h.invert.freeze
+    }.call
 
     ID_NAME_MAP = NAME_ID_MAP.invert.freeze
 
